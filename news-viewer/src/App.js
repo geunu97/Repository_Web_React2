ebf1c3c2415d99948bdb6737e8299898232d577d
olo90632951 <olo90632951@gmail.com>
2022-02-07 19:51:48 +0900

    ..

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,30 +1,8 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
